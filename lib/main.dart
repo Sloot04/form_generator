dd752c7e3748af7cd6e5c8bd96416e2ea3197722
Le127 <nereocarbonara@gmail.com>
2022-01-06 13:09:05 -0300

    firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:form_generator/bloc/card_bloc.dart';
 import 'package:form_generator/bloc/observer.dart';
@@ -8,11 +11,13 @@ import 'package:form_generator/pages/form_generator_page.dart';
 import 'package:form_generator/pages/form_page.dart';
 import 'package:form_generator/pages/main_page.dart';
 
-void main() {
-  BlocOverrides.runZoned(() => runApp(const MyApp()),
-   blocObserver: StateObserver()
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
-
+  BlocOverrides.runZoned(() => runApp(const MyApp()),
+      blocObserver: StateObserver());
 }
 
 class MyApp extends StatelessWidget {
@@ -30,7 +35,7 @@ class MyApp extends StatelessWidget {
           '/': (_) => const MainPage(),
           '/form_generator': (_) => const FormGeneratorPage(),
           '/form': (_) => const FormPage(),
-          '/detail': (_)=> const DetailCoursePage(),
+          '/detail': (_) => const DetailCoursePage(),
         },
       ),
     );
